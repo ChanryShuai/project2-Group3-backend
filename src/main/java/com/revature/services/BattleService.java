3dedf95d07f7178fc17deeb7061f8da9c57bfd08
Cadie Limbaker <cadielimbaker@gmail.com>
2020-09-16 00:33:42 -0400

    DAOS and Services are pretty much done

--- src/main/java/com/revature/services/BattleService.java
+++ src/main/java/com/revature/services/BattleService.java
@@ -1,5 +1,36 @@
 package com.revature.services;
 
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.revature.repositories.IBattleDAO;
+import com.revature.models.Battle;
+import com.revature.repositories.BattleDAO;
+
 public class BattleService {
 
+	private static IBattleDAO bdao = new BattleDAO();
+	private static final Logger log = LogManager.getLogger(BattleService.class);
+	
+	public void findOutcome(int battleId) {
+		log.info("Finding the battle outcome");
+		bdao.findOutcome(battleId);
+	}
+	
+	public boolean updateBattle(Battle b) {
+		log.info("Updating the user's battle info");
+		return bdao.updateBattle(b);
+	}
+	
+	public List<Battle> findAllBattles(){
+		log.info("Finding all the battles");
+		return bdao.findAllBattles();
+	}
+	
+	public List<Battle> findBattlesByUser(int userId){
+		log.info("Finding the user's battles");
+		return bdao.findBattlesByUser(userId);
+	}
 }
--- src/main/java/com/revature/services/SuperheroService.java
+++ src/main/java/com/revature/services/SuperheroService.java
@@ -4,6 +4,8 @@ import com.revature.models.AddSuperheroDTO;
 import com.revature.models.Superhero;
 import com.revature.repositories.*;
 
+import java.util.List;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -28,5 +30,33 @@ public class SuperheroService {
 //		return false;
 //	}
 
-	public boolean addSuperhero
+	public boolean addSuperhero (Superhero s) {
+		log.info("Adding a Superhero from the API");
+		return sdao.addSuperhero(s);
+	}
+	
+	public List<Superhero> findAllSuperheros(){
+		log.info("Finding all the superheros");
+		return sdao.findAllSuperheros();
+	}
+	
+	public Superhero findById(int id) {
+		log.info("Finding a superhero from the id");
+		return sdao.findById(id);
+	}
+	
+	public List<Superhero> findByAlignment(String alignment){
+		log.info("Find all the superheros by alignment");
+		return sdao.findByAlignment(alignment);
+	}
+	
+	public List<Superhero> random5(String alignment){
+		log.info("Giving the user a set of 5 superheros");
+		return sdao.random5(alignment);
+	}
+	
+	public Superhero randomOpponent(String alignment) {
+		log.info("Giving the user an opponent");
+		return sdao.randomOpponent(alignment);
+	}
 }
