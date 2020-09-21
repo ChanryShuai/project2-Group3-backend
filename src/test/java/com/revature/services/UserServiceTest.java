771caebaca230b0f2011863083af0376d114a84f
unknown <annabelladoncell@gmail.com>
2020-09-21 14:51:01 -0400

    9/21 2:50pm UserServiceTest complete

--- src/test/java/com/revature/services/UserServiceTest.java
+++ src/test/java/com/revature/services/UserServiceTest.java
@@ -6,6 +6,9 @@ import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
 import com.revature.models.User;
@@ -57,6 +60,64 @@ public class UserServiceTest {
 
 	}
 	
+	@Test
+	public void testfindByUsernameNull() {
+		when(udaoMock.findByUsername("test")).thenReturn(null);
+		assertEquals(null, uSerMock.findByUsername("test"));
+	}
 	
+	@Test
+	public void testfindByUsername() {
+		User u = new User();
+		u.setUsername("test");
+		when(udaoMock.findByUsername("test")).thenReturn(u);
+		assertEquals(u, uSerMock.findByUsername("test"));
+	}
+	
+	@Test
+	public void testUpdateUser() {
+		User u = new User();
+		when(udaoMock.updateUser(u)).thenReturn(u);
+		assertEquals(u, uSerMock.updateUser(u));
+	}
+	
+	@Test
+	public void testUpdateUserNull() {
+		User u = null;
+		when(udaoMock.updateUser(u)).thenReturn(null);
+		assertEquals(null, uSerMock.updateUser(u));
+	}
+	
+	@Test
+	public void testSelectByUserId() {
+		User u = new User();
+		u.setUserId(3);
+		when(udaoMock.selectByUserId(3)).thenReturn(u);
+		assertEquals(u, uSerMock.selectByUserId(3));
+	}
+	
+	public void testSelectByUserIdNull() {
+		when(udaoMock.selectByUserId(3)).thenReturn(null);
+		assertEquals(null, uSerMock.selectByUserId(3));
+	}
+	
+	public void testFindAllUsers() {
+		User u1 = new User();
+		User u2 = new User();
+		User u3 = new User();
+		
+		List<User> uList = new ArrayList<User>();
+		uList.add(u1);
+		uList.add(u2);
+		uList.add(u3);
+		
+		when(udaoMock.findAllUsers()).thenReturn(uList);
+		assertEquals(uList, uSerMock.findAllUsers());
+	}
+	
+	public void testFindAllUsersEmpty() {
+		when(udaoMock.findAllUsers()).thenReturn(null);
+		assertEquals(null, uSerMock.findAllUsers());
+	}
 	
 }
